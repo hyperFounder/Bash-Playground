9fbb05617198f39df733c95ef601c2b828f01905
hyperFounder <RXR274@student.bham.ac.uk>
2023-09-12 11:36:54 +0100

    Remove ignored files

---
+++ bash_exercises.sh
@@ -0,0 +1,13 @@
+# Create a Bash script that takes a text file as an argument and counts the number of words in the file.
+#Display the word count at the end.
+
+function textfile() {
+    # Get the name of the Bash script file
+    local file=$1
+    first_char="${file:0:1}"
+    wc -w $file | cut -d "$first_char" -f1; echo "words"
+}
+
+echo -n "Please input a file name: "
+read -r userinput
+textfile $userinput
